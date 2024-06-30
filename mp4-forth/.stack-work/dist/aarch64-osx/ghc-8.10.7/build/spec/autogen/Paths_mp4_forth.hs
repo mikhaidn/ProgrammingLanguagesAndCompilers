a19eda9306b659b6d16c0ecdd1763012d7148068
Dan M <danmikhail@gmail.com>
2024-06-29 22:28:09 -0400

    finish mp4

---
+++ mp4-forth/.stack-work/dist/aarch64-osx/ghc-8.10.7/build/spec/autogen/Paths_mp4_forth.hs
@@ -0,0 +1,50 @@
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE NoRebindableSyntax #-}
+{-# OPTIONS_GHC -fno-warn-missing-import-lists #-}
+module Paths_mp4_forth (
+    version,
+    getBinDir, getLibDir, getDynLibDir, getDataDir, getLibexecDir,
+    getDataFileName, getSysconfDir
+  ) where
+
+import qualified Control.Exception as Exception
+import Data.Version (Version(..))
+import System.Environment (getEnv)
+import Prelude
+
+#if defined(VERSION_base)
+
+#if MIN_VERSION_base(4,0,0)
+catchIO :: IO a -> (Exception.IOException -> IO a) -> IO a
+#else
+catchIO :: IO a -> (Exception.Exception -> IO a) -> IO a
+#endif
+
+#else
+catchIO :: IO a -> (Exception.IOException -> IO a) -> IO a
+#endif
+catchIO = Exception.catch
+
+version :: Version
+version = Version [0,1,0,0] []
+bindir, libdir, dynlibdir, datadir, libexecdir, sysconfdir :: FilePath
+
+bindir     = "/Users/danmikhail/git/ProgrammingLanguagesAndCompilers/mp4-forth/.stack-work/install/aarch64-osx/75a682f6f2fa22269098ed73fc07efe5d8a0059ad58ca9ceb4aa12f3449b8bf3/8.10.7/bin"
+libdir     = "/Users/danmikhail/git/ProgrammingLanguagesAndCompilers/mp4-forth/.stack-work/install/aarch64-osx/75a682f6f2fa22269098ed73fc07efe5d8a0059ad58ca9ceb4aa12f3449b8bf3/8.10.7/lib/aarch64-osx-ghc-8.10.7/mp4-forth-0.1.0.0-7Xcd7cZzGeK8oOXoZzkPAP-spec"
+dynlibdir  = "/Users/danmikhail/git/ProgrammingLanguagesAndCompilers/mp4-forth/.stack-work/install/aarch64-osx/75a682f6f2fa22269098ed73fc07efe5d8a0059ad58ca9ceb4aa12f3449b8bf3/8.10.7/lib/aarch64-osx-ghc-8.10.7"
+datadir    = "/Users/danmikhail/git/ProgrammingLanguagesAndCompilers/mp4-forth/.stack-work/install/aarch64-osx/75a682f6f2fa22269098ed73fc07efe5d8a0059ad58ca9ceb4aa12f3449b8bf3/8.10.7/share/aarch64-osx-ghc-8.10.7/mp4-forth-0.1.0.0"
+libexecdir = "/Users/danmikhail/git/ProgrammingLanguagesAndCompilers/mp4-forth/.stack-work/install/aarch64-osx/75a682f6f2fa22269098ed73fc07efe5d8a0059ad58ca9ceb4aa12f3449b8bf3/8.10.7/libexec/aarch64-osx-ghc-8.10.7/mp4-forth-0.1.0.0"
+sysconfdir = "/Users/danmikhail/git/ProgrammingLanguagesAndCompilers/mp4-forth/.stack-work/install/aarch64-osx/75a682f6f2fa22269098ed73fc07efe5d8a0059ad58ca9ceb4aa12f3449b8bf3/8.10.7/etc"
+
+getBinDir, getLibDir, getDynLibDir, getDataDir, getLibexecDir, getSysconfDir :: IO FilePath
+getBinDir = catchIO (getEnv "mp4_forth_bindir") (\_ -> return bindir)
+getLibDir = catchIO (getEnv "mp4_forth_libdir") (\_ -> return libdir)
+getDynLibDir = catchIO (getEnv "mp4_forth_dynlibdir") (\_ -> return dynlibdir)
+getDataDir = catchIO (getEnv "mp4_forth_datadir") (\_ -> return datadir)
+getLibexecDir = catchIO (getEnv "mp4_forth_libexecdir") (\_ -> return libexecdir)
+getSysconfDir = catchIO (getEnv "mp4_forth_sysconfdir") (\_ -> return sysconfdir)
+
+getDataFileName :: FilePath -> IO FilePath
+getDataFileName name = do
+  dir <- getDataDir
+  return (dir ++ "/" ++ name)
--- mp4-forth/src/Lib.hs
+++ mp4-forth/src/Lib.hs
@@ -1,9 +1,13 @@
 --- Given Library Code
 --- ==================
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
+{-# HLINT ignore "Redundant bracket" #-}
 
 module Lib where
 
 type ErrorMsg = String
+
 -- for stack underflow errors
 msgUnderflow :: ErrorMsg
 msgUnderflow = "Stack underflow"
@@ -31,27 +35,32 @@ underflow = error msgUnderflow
 --- ---------
 
 type ForthState = (IStack, Dictionary, Output)
+
 type Transition = (ForthState -> ForthState)
-type IStack     = [Integer]
+
+type IStack = [Integer]
+
 type Dictionary = [(String, Value)]
-type Output     = [String]
 
-type CStack     = [(String, Transition)]
+type Output = [String]
 
-data Value = Prim Transition
-           | Define
-           | EndDef
-           | Compile (CStack -> Maybe CStack)
-           | Num Integer
-           | Unknown String
+type CStack = [(String, Transition)]
+
+data Value
+  = Prim Transition
+  | Define
+  | EndDef
+  | Compile (CStack -> Maybe CStack)
+  | Num Integer
+  | Unknown String
 
 instance Show Value where
-    show (Prim f)    = "Prim"
-    show (Compile _) = msgCompileOnly
-    show Define      = "Define"
-    show EndDef      = msgCompileOnly
-    show (Num i)     = show i
-    show (Unknown s) = msgUndefinedSym s
+  show (Prim f) = "Prim"
+  show (Compile _) = msgCompileOnly
+  show Define = "Define"
+  show EndDef = msgCompileOnly
+  show (Num i) = show i
+  show (Unknown s) = msgUndefinedSym s
 
 --- Dictionary Access
 --- -----------------
@@ -60,18 +69,18 @@ instance Show Value where
 
 -- handle input lookups (and integers)
 dlookup :: String -> Dictionary -> Value
-dlookup word dict
-    = case lookup word dict of
-        Just x -> x
-        _          -> case reads word of
-                        [(i,"")] -> Num i
-                        _        -> Unknown word
+dlookup word dict =
+  case lookup word dict of
+    Just x -> x
+    _ -> case reads word of
+      [(i, "")] -> Num i
+      _ -> Unknown word
 
 --- ### Insert
 
 -- handle inserting things into the dictionary
 dinsert :: String -> Value -> Dictionary -> Dictionary
-dinsert key val dict = (key, val):dict
+dinsert key val dict = (key, val) : dict
 
 --- Problems
 --- ========
@@ -80,155 +89,201 @@ dinsert key val dict = (key, val):dict
 --- -------
 
 liftIStackOp :: (IStack -> Maybe IStack) -> ForthState -> ForthState
-liftIStackOp op (i, d, o)
-    = case op i of
-        Just i' -> (i', d, o)
-        Nothing -> underflow
+liftIStackOp op (i, d, o) =
+  case op i of
+    Just i' -> (i', d, o)
+    Nothing -> underflow
 
 --- ### `liftIntOp`
 
 liftIntOp :: (Integer -> Integer -> Integer) -> IStack -> Maybe IStack
-liftIntOp op (x:y:xs) = Just $ (y `op` x) : xs
-liftIntOp _  _        = Nothing
+liftIntOp op (x : y : xs) = Just $ (y `op` x) : xs
+liftIntOp _ _ = Nothing
 
 --- ### `liftCompOp`
 
 liftCompOp :: (Integer -> Integer -> Bool) -> IStack -> Maybe IStack
-liftCompOp = undefined
-
+liftCompOp op (x : y : xs) = if y `op` x then Just $ -1 : xs else Just $ 0 : xs
+liftCompOp _ _ = Nothing
 
 --- The Dictionary
 --- --------------
 
 initialDictionary :: Dictionary
-initialDictionary = initArith ++ initComp ++ initIStackOp ++ initPrintOp
-                 ++ initCompileOp
+initialDictionary =
+  initArith ++ initComp ++ initIStackOp ++ initPrintOp
+    ++ initCompileOp
 
-initCompileOp = [ (":",    Define)
-                , (";",    EndDef)
-                , ("for",  Compile cstackFor)
-                , ("next", Compile cstackNext)
-                , ("if",   Compile cstackIf)
-                , ("else", Compile cstackElse)
-                , ("then", Compile cstackThen)
-                , ("begin",Compile cstackBegin)
-                , ("until",Compile cstackUntil)
-                ]
+initCompileOp =
+  [ (":", Define),
+    (";", EndDef),
+    ("for", Compile cstackFor),
+    ("next", Compile cstackNext),
+    ("if", Compile cstackIf),
+    ("else", Compile cstackElse),
+    ("then", Compile cstackThen),
+    ("begin", Compile cstackBegin),
+    ("until", Compile cstackUntil)
+  ]
 
 --- ### Arithmetic Operators
 
 initArith :: Dictionary
-initArith = [ ("+",  Prim $ liftIStackOp $ liftIntOp (+))
-            ]
+initArith =
+  [ ("+", Prim $ liftIStackOp $ liftIntOp (+)),
+    ("-", Prim $ liftIStackOp $ liftIntOp (-)),
+    ("*", Prim $ liftIStackOp $ liftIntOp (*)),
+    ("/", Prim $ liftIStackOp $ liftIntOp div)
+  ]
 
 --- ### Comparison Operators
 
 initComp :: Dictionary
-initComp = []
+initComp =
+  [ (">", Prim $ liftIStackOp $ liftCompOp (>)),
+    ("<", Prim $ liftIStackOp $ liftCompOp (<)),
+    ("<=", Prim $ liftIStackOp $ liftCompOp (<=)),
+    (">=", Prim $ liftIStackOp $ liftCompOp (>=)),
+    ("=", Prim $ liftIStackOp $ liftCompOp (==)),
+    ("!=", Prim $ liftIStackOp $ liftCompOp (/=))
+  ]
 
 --- ### Stack Manipulations
 
 initIStackOp :: Dictionary
-initIStackOp = [ ("dup",  Prim $ liftIStackOp istackDup)
-               ]
+initIStackOp =
+  [ ("dup", Prim $ liftIStackOp istackDup),
+    ("drop", Prim $ liftIStackOp istackDrop),
+    ("swap", Prim $ liftIStackOp istackSwap),
+    ("rot", Prim $ liftIStackOp istackRot)
+  ]
 
-initPrintOp = [ (".",  Prim printPop)
-              ]
+initPrintOp =
+  [ (".", Prim printPop),
+    (".S", Prim printStack)
+  ]
 
 istackDup :: IStack -> Maybe IStack
-istackDup (i:is) = Just $ i:i:is
-istackDup _      = Nothing
+istackDup (i : is) = Just $ i : i : is
+istackDup _ = Nothing
 
 istackSwap :: IStack -> Maybe IStack
-istackSwap = undefined
+istackSwap (i : j : is) = Just $ j : i : is
+istackSwap _ = Nothing
 
 istackDrop :: IStack -> Maybe IStack
-istackDrop = undefined
+istackDrop (i : is) = Just $ is
+istackDrop _ = Nothing
 
 istackRot :: IStack -> Maybe IStack
-istackRot = undefined
+istackRot (i : j : k : is) = Just $ k : i : j : is
+istackRot _ = Nothing
 
 --- ### Popping the Stack
 
 printPop :: ForthState -> ForthState
-printPop (i:istack, dict, out) =
-    (istack, dict, show i : out)
+printPop (i : istack, dict, out) =
+  (istack, dict, show i : out)
 printPop _ = underflow
 
 --- ### Printing the Stack
 
 printStack :: ForthState -> ForthState
-printStack (istack, dict, out) = undefined
+printStack (istack, dict, out) = (istack, dict, (unwords (map show (reverse istack))) : out)
 
 --- Evaluator
 --- ---------
 
 eval :: [String] -> ForthState -> ForthState
-
 -- empty input -> return current state and output "ok"
-eval [] (istack, dict, out) = (istack, dict, "ok":out)
-
+eval [] (istack, dict, out) = (istack, dict, "ok" : out)
 --- ### Lookup in dictionary
 
 -- otherwise it should be handled by `dlookup` to see if it's a `Num`, `Prim`,
 -- `Define`, `Compile`, or `Unknown`
-eval (w:ws) state@(istack, dict, out)
-    = case dlookup w dict of
-        Prim f    -> eval ws (f state)
-        Num i     -> eval ws (i:istack, dict, out)
-        Define    ->
-            case compileDef ws dict of
-                Right (rest, dict') -> eval rest (istack, dict', out)
-                Left msg -> ([], dict, msg:out)
-        otherwise -> -- reset IStack and add error message
-            ([], dict, (show otherwise):out)
+eval (w : ws) state@(istack, dict, out) =
+  case dlookup w dict of
+    Prim f -> eval ws (f state)
+    Num i -> eval ws (i : istack, dict, out)
+    Define ->
+      case compileDef ws dict of
+        Right (rest, dict') -> eval rest (istack, dict', out)
+        Left msg -> ([], dict, msg : out)
+    otherwise ->
+      -- reset IStack and add error message
+      ([], dict, (show otherwise) : out)
 
 --- Compiler
 --- --------
 
 --- ### Definite Loops
 transForLoop :: Transition -> (ForthState -> ForthState)
-transForLoop kloop (i:is, d, o) =
-    if i < 0 then
-        (is, d, o)
-    else
-        (aux kloop i) (is, d, o)
-        where aux k 0 = k
-              aux k n = k . (aux k (n-1))
+transForLoop kloop (i : is, d, o) =
+  if i < 0
+    then (is, d, o)
+    else (aux kloop i) (is, d, o)
+  where
+    aux k 0 = k
+    aux k n = k . (aux k (n -1))
 transForLoop _ _ = underflow
 
-
 cstackFor :: CStack -> Maybe CStack
-cstackFor cstack = Just $ ("for", id):cstack
+cstackFor cstack = Just $ ("for", id) : cstack
 
 cstackNext :: CStack -> Maybe CStack
-cstackNext (("for", kloop):(c, kold):cstack) =
-    Just ((c, knew):cstack) where knew = (transForLoop kloop) . kold
+cstackNext (("for", kloop) : (c, kold) : cstack) =
+  Just ((c, knew) : cstack)
+  where
+    knew = (transForLoop kloop) . kold
 cstackNext _ = Nothing
 
 --- ### Conditionals
 
 cstackIf :: CStack -> Maybe CStack
-cstackIf cstack = undefined
+cstackIf cstack = Just $ ("if", id) : cstack
 
 cstackElse :: CStack -> Maybe CStack
-cstackElse cstack@(("if", _):_) = undefined
+cstackElse cstack@(("if", kif) : _) = Just $ ("else", id) : cstack
 cstackElse _ = Nothing
 
+-- I don't understand *why* this works, I tried to avoid using trans* vs in-line implementation
+transConditional :: Transition -> Transition -> (ForthState -> ForthState)
+transConditional kif kelse (i : is, d, o) =
+  if i /= 0
+    then kif (is, d, o)
+    else kelse (is, d, o)
+transConditional _ _ _ = underflow
+
 cstackThen :: CStack -> Maybe CStack
-cstackThen (("else", kelse):("if", kif):(c, kold):cstack) = undefined
-cstackThen (("if", kif):(c, kold):cstack) = undefined
+cstackThen (("else", kelse) : ("if", kif) : (c, kold) : cstack) =
+  Just $ (c, knew) : cstack
+  where
+    knew = (transConditional kif kelse) . kold
+cstackThen (("if", kif) : (c, kold) : cstack) =
+  Just $ (c, knew) : cstack
+  where
+    knew = (transConditional kif id) . kold
 cstackThen _ = Nothing
 
 --- ### Indefinite Loops
-
 cstackBegin :: CStack -> Maybe CStack
-cstackBegin cstack = Just $ ("begin", id):cstack
+cstackBegin cstack = Just $ ("begin", id) : cstack
+
+transUntil :: Transition -> (ForthState -> ForthState)
+transUntil kloop state = aux (kloop state)
+  where
+    aux st@(i : is, d, o) =
+      if i /= 0
+      then (is, d, o) 
+      else aux (kloop (is, d, o)) 
+    aux _ = underflow  
 
 cstackUntil :: CStack -> Maybe CStack
-cstackUntil (("begin", kloop):(c, kold):cstack) = undefined
+cstackUntil (("begin", kloop) : (c, kold) : cstack) =
+  Just $ (c, knew) : cstack
+  where
+    knew = (transUntil kloop) . kold
 cstackUntil _ = Nothing
-
 
 --- ### Lookup in dictionary
 
@@ -236,30 +291,32 @@ cstackUntil _ = Nothing
 -- Assuming ':' is already stripped away
 compileDef :: [String] -> Dictionary -> Either ErrorMsg ([String], Dictionary)
 compileDef [] _ = Left msgZeroLenDef
-compileDef (name:ws) dict
-    = case  compile ws dict [("", id)] of
-       Right (rest, f) -> Right (rest, dinsert name (Prim f) dict)
-       Left msg -> Left msg
+compileDef (name : ws) dict =
+  case compile ws dict [("", id)] of
+    Right (rest, f) -> Right (rest, dinsert name (Prim f) dict)
+    Left msg -> Left msg
 
-compile :: [String] -> Dictionary -> CStack
-           -> Either ErrorMsg ([String], Transition)
+compile ::
+  [String] ->
+  Dictionary ->
+  CStack ->
+  Either ErrorMsg ([String], Transition)
 compile [] _ _ = Left "The definition does not end"
-
-compile (w:ws) dict cstack
-    = case dlookup w dict of
-        Prim f    -> compile ws dict (updateTop f cstack)
-        Num i     -> compile ws dict (updateTop f cstack)
-                     where f = (liftIStackOp (\is -> Just (i:is)))
-        Compile cf-> case cf cstack of
-                Just cstack' -> compile ws dict cstack'
-                Nothing -> Left $ msgUnstructured w
-        Define    -> Left "Nested definition is not allowed"
-        EndDef    -> case cstack of
-            [("", k)] -> Right (ws, k)
-            otherwise -> Left $ msgUnstructured w
-        otherwise -> Left $ show otherwise
+compile (w : ws) dict cstack =
+  case dlookup w dict of
+    Prim f -> compile ws dict (updateTop f cstack)
+    Num i -> compile ws dict (updateTop f cstack)
+      where
+        f = (liftIStackOp (\is -> Just (i : is)))
+    Compile cf -> case cf cstack of
+      Just cstack' -> compile ws dict cstack'
+      Nothing -> Left $ msgUnstructured w
+    Define -> Left "Nested definition is not allowed"
+    EndDef -> case cstack of
+      [("", k)] -> Right (ws, k)
+      otherwise -> Left $ msgUnstructured w
+    otherwise -> Left $ show otherwise
 
 updateTop :: Transition -> CStack -> CStack
-updateTop k ((c, kold):cs) = (c, k . kold) : cs
+updateTop k ((c, kold) : cs) = (c, k . kold) : cs
 updateTop _ [] = underflow
-
